caed4dda5a55d780c34c9ff3ee5b79d09632ce6b
Yajat Dureja <yajatdureja@gmail.com>
2024-08-05 14:42:32 +0530

    mongo db atlas url

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 try {
-  mongoose.connect("mongodb://localhost:27017/Ecommerce", {
+  mongoose.connect("mongodb+srv://yd:yd123@cluster0.a6jojvd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
